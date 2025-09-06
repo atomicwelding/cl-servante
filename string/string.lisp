f370d3d12b480fc9b3a6d204b2de36dcc235a0e4
atomicwelding <ctfweld@gmail.com>
2025-09-06 23:55:48 +0200

    add splice method

--- string/string.lisp
+++ string/string.lisp
@@ -1,5 +1,7 @@
 (defpackage #:cl-servante.string
-  (:use #:cl))
+  (:use #:cl)
+  (:export #:starts-with-p
+	   #:slice))
 
 
 (in-package #:cl-servante.string)
@@ -7,3 +9,7 @@
 (defun starts-with-p (s pattern &optional (test #'char=))
   (let ((pos (search pattern s :test test)))
     (and pos (= pos 0))))
+
+
+(defun slice (start end s)
+  (subseq s start (min (length s) end)))
